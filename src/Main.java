158274916abd89e26afbf2a42be3c4c76e86e3dd
Khrystyna2023 <zagorovsk@gmail.com>
2023-04-02 17:10:29 +0300

    HomeWork 4.Created two classes Main and Print. Print class receives, prints and returns int value. Main clas - printing returning value

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Print myPrint = new Print();
+        System.out.println("\n" + "Returning value:"+ "\n" + "~~~" + Print.myPrint() + "~~~");
+    }
+}
---
+++ src/Print.java
@@ -0,0 +1,27 @@
+import java.util.PrimitiveIterator;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner; //імпорт сканера (використовується для ввода даних з клави)
+
+public class Print {
+    public static void main(String[] args) {
+        }
+        public static int myPrint () {
+            Scanner UserInput = new Scanner(System.in);  //цей код дозволяє користувачеві читати число з System.in
+            System.out.println("Please, enter only integer number p:");
+            if(UserInput.hasNextInt()){
+                int p = UserInput.nextInt();  //дозволяє ввести любе ціле число
+                System.out.println("---" + " " + p + " " + "---");
+                return p;
+            }
+            else
+                System.out.println("Incorrect value. Please, enter only integer number");
+            int i = myPrint();
+            return i;
+        }
+
+    }
+
+
+
